36b57e8a3fef5298cd6c9afb06fe617a985220a4
ishubhamsingh <coolsks94@gmail.com>
2015-12-17 05:30:48 +0530

    dt for mm6.0
    Change-Id: Ic4211936fed65f4cd9236ea24fc4832285452f88

--- aicp.mk
+++ aicp.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
 PRODUCT_NAME := cm_titan
