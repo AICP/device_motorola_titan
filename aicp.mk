f13a7ed857d8243a67c9bbb11a990de61335febd
ishubhamsingh <coolsks94@gmail.com>
2015-12-20 15:35:43 +0530

    Updated aicp.mk
    Change-Id: I97063cf9e3a1a0653ca0b287ae26dbbdd3e3c75e

--- aicp.mk
+++ aicp.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := cm_titan
+PRODUCT_NAME := aicp_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
